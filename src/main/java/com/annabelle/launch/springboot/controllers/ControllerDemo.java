52a03b7db3ec8a67499d5864b8948330aa6d5e98
EllisAC <ellisac@jncb.com>
2022-01-13 10:27:20 -0500

    Display Name Variables from ArrayList in names.html

--- src/main/java/com/annabelle/launch/springboot/controllers/ControllerDemo.java
+++ src/main/java/com/annabelle/launch/springboot/controllers/ControllerDemo.java
@@ -5,9 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 
 /**
  *      Created by: Annabelle Ellis
+ *      https://www.youtube.com/watch?v=0z_hjs5XRy0&list=PLs5n5nYB22fI83_UAFbPLC-Mg9Uc6jeU4&index=9
  */
 
 @Controller
@@ -20,9 +23,21 @@ public class ControllerDemo {
     @GetMapping("/")
     public String index(Model model){
 //        Takes two parameters of a key value pair
-        model.addAttribute("param","attributePassed");
+        model.addAttribute("parameter","attributePassed");
         return "views/index";
     }
 
+
+    @GetMapping("/names")
+    public String namesArr(Model model){
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Anna");
+        names.add("Adjaynae");
+        names.add("Kaela");
+        names.add("Khrisante");
+
+        model.addAttribute("namesArray",names);
+        return "views/names";
+    }
 
 }
